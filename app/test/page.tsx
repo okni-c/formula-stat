762d2286b05df4e2f7cd8e7926345bc55f9a2d61
Dallas Yatsinko <oknistay@gmail.com>
2023-07-14 15:54:01 -0500

    Remove default styles

---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function Page() {
+    return(
+        <div>
+            <h2>Test page</h2>
+            <Link href='/'>Back to home</Link>
+        </div>
+    )
+}
