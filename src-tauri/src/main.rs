e38468b895e116eeeda63d1a055fcfef08db15b2
mucus <mucus>
2023-07-14 18:11:50 -0500

    Testing Objects

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,17 @@
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use serde::Serialize;
+#[derive(Serialize)]
+struct Record {
+  id: i8,
+  source: String,
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, get_data])
+        .invoke_handler(tauri::generate_handler![greet, get_data, send_data])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
@@ -16,4 +24,13 @@ fn greet(name: &str) -> String {
 #[tauri::command]
 fn get_data() -> String {
   "This data was sent from the back end only.".into()
+}
+
+#[tauri::command]
+fn send_data() -> Record {
+  let r1 = Record {
+    id: 1,
+    source: String::from("Opal"),
+  };
+  return r1;
 }
