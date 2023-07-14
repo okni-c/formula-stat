dbe2e4a123debd4087bdc0e79c68b2008e7076fc
mucus <mucus>
2023-07-14 18:58:22 -0500

    Creates 100 records

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,18 +2,29 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use serde::Serialize;
-#[derive(Serialize)]
+use serde::{Serialize,Deserialize};
+use rand::{seq::SliceRandom};
+
+#[derive(Serialize,Clone,Deserialize)]
 struct Record {
   id: i8,
   source: String,
 }
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet, get_data, send_data, send_all_data])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  let mut people: Vec<Record> = Vec::new();
+
+  let mut i:i8 = 0;
+  for _ in 0..100{
+    let record: Record = create_record(i);
+    people.push(record);
+    i += 1;
+  }
+
+  tauri::Builder::default()
+      .invoke_handler(tauri::generate_handler![greet, get_data, send_data, send_all_data])
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
 }
 
 #[tauri::command]
@@ -36,16 +47,16 @@ fn send_data() -> Record {
 }
 
 #[tauri::command]
-fn send_all_data() -> Vec<Record> {
-  let r1 = Record {
-    id: 1,
-    source: String::from("Opal"),
-  };
-  let r2 = Record {
-    id: 2,
-    source: String::from("Opal Baby"),
-  };
+fn send_all_data(people: Vec<Record>) -> Vec<Record> {
+  return people;
+}
 
-  let x: Vec<Record> = vec![r1,r2];
-  return x;
+fn create_record(id:i8) -> Record {
+  let names: Vec<&str> = vec!["Opal","Marc","Dallas"];
+  let name: String = names.choose(&mut rand::thread_rng()).unwrap().to_string();
+  let record: Record = Record{
+    id:id,
+    source:name,
+  };
+  return record;
 }
