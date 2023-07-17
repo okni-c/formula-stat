1134f3ab10acd9541919901238d38bfd0565d5c1
mucus <mucus>
2023-07-16 20:59:00 -0500

    Created SQL Table F1_db; Tables: Races, Circuits; Drivers

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,30 +1,27 @@
+/*
+TODO:
+* [] Create Races Database from csv
+*/
+
 
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use serde::{Serialize,Deserialize};
-use rand::{seq::SliceRandom};
+use std::error::Error;
+mod database;
 
-#[derive(Serialize,Clone,Deserialize)]
-struct Record {
-  id: i8,
-  source: String,
-}
-
-fn main() {
-  let mut people: Vec<Record> = Vec::new();
-
-  let mut i:i8 = 0;
-  for _ in 0..100{
-    let record: Record = create_record(i);
-    people.push(record);
-    i += 1;
-  }
+fn main() -> Result<(), Box<dyn Error>> {
+  database::create_tables()?;
+  database::populate_tables_via_csv()?;
+  let x: Vec<database::Races> = database::get_races(String::from("2023"))?;
+  println!("{:?}",x);
 
   tauri::Builder::default()
-      .invoke_handler(tauri::generate_handler![greet, get_data, send_data, send_all_data])
+      .invoke_handler(tauri::generate_handler![greet])
       .run(tauri::generate_context!())
       .expect("error while running tauri application");
+
+  Ok(())
 }
 
 #[tauri::command]
@@ -32,31 +29,4 @@ fn greet(name: &str) -> String {
     format!("Here is your string from the front end: {}", name)
 }
 
-#[tauri::command]
-fn get_data() -> String {
-  "This data was sent from the back end only.".into()
-}
 
-#[tauri::command]
-fn send_data() -> Record {
-  let r1 = Record {
-    id: 1,
-    source: String::from("Opal"),
-  };
-  return r1;
-}
-
-#[tauri::command]
-fn send_all_data(people: Vec<Record>) -> Vec<Record> {
-  return people;
-}
-
-fn create_record(id:i8) -> Record {
-  let names: Vec<&str> = vec!["Opal","Marc","Dallas"];
-  let name: String = names.choose(&mut rand::thread_rng()).unwrap().to_string();
-  let record: Record = Record{
-    id:id,
-    source:name,
-  };
-  return record;
-}
